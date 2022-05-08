973843efd5cdd44aaba542fb31a75ce2b2c0f407
Mohuya748 <khandakermohuya@gmail.com>
2022-05-08 22:17:09 +0600

    delete button

--- src/Pages/MyItems/MyItems.js
+++ src/Pages/MyItems/MyItems.js
@@ -23,22 +23,23 @@ const MyItems = () => {
             
 
 
-    // const handleItemDelete = id => {
-    //     const proceed = window.confirm('Deleting Items is Permanent! Think twice before pressing OK...');
-    //     if (proceed) {
-    //         const url = `http://https://enigmatic-tundra-16228.herokuapp.com/inventory0/item/${id}`;
-    //         fetch(url, {
-    //             method: 'DELETE'
-    //         })
-    //             .then(res => res.json())
-    //             .then(data => {
-    //                 if (data.deletedCount > 0) {
-    //                     const remaining = myItems.filter(item => item._id !== id);
-    //                     setMyItems(remaining);
-    //                 }
-    //             })
-    //     }
-    // }
+    const handleItemDelete = id => {
+        const proceed = window.confirm('Deleting Items is Permanent! Think twice before pressing OK...');
+        if (proceed) {
+            const email = user.email;
+            const url = `https://enigmatic-tundra-16228.herokuapp.com/myitems?email=${email}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        const remaining = myItems.filter(item => item._id !== id);
+                        setMyItems(remaining);
+                    }
+                })
+        }
+    }
 
 
       
@@ -47,17 +48,17 @@ const MyItems = () => {
         <div className='container'>
         <h2 className='fw-bold text-center mt-5'>{myItems.length}</h2>
         <div className='row  mb-5'>{
-            myItems.map(item => <div key={myItems._id}>
+            myItems.map(item => <div key={item._id}>
                 <div className="col col-sm-12 col-lg-4 container mb-4">
                     <div className="card h-100">
-                        <img src={myItems.img} className="card-img-top" alt="..." />
+                        <img src={item.img} className="card-img-top" alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">{myItems.name}</h5>
-                            <p className="card-text">Price: {myItems.price}BDT/kg</p>
-                            <p className="card-text">Description: {myItems.description}</p>
-                            <p className="card-text">Supplier: {myItems.Supplier}</p>
-                            <p className="card-text">Quantity: {myItems.Quantity} kg</p>
-                            {/* <button onClick={() => handleItemDelete(myItems._id)}>DELETE</button> */}
+                            <h5 className="card-title">{item.name}</h5>
+                            <p className="card-text">Price: {item.price}BDT/kg</p>
+                            <p className="card-text">Description: {item.description}</p>
+                            <p className="card-text">Supplier: {item.Supplier}</p>
+                            <p className="card-text">Quantity: {item.Quantity} kg</p>
+                            <button onClick={() => handleItemDelete(myItems._id)}>DELETE</button>
                         </div>
                     </div>
                 </div>
