2a4e2aa5f4e4781fa29264c26e2e3fbe62a3d78a
Mohuya748 <khandakermohuya@gmail.com>
2022-05-08 10:59:03 +0600

    about added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-                <h2>this is about</h2>
+        <div className='d-flex flex-wrap container text-center'>
+            <div className='m-4'>
+                <h2>First Choice Fruits</h2>
+                <p>This a Fruits Warehouse Named First Choice Fruits. It's basically warehousehouse management system. Here you can keep calculation of your warehouse products. And with it's help you can calculate your produucts so easily.</p>
+            </div>
+            <img className='w-50 mx-auto' src="https://media.istockphoto.com/photos/farm-worker-prepares-wooden-pallets-with-strawberries-for-the-market-picture-id1254189237?b=1&k=20&m=1254189237&s=170667a&w=0&h=aC_gizZlL-AZzMI0Yj-S2dFsiV5i0-hMFRvHG7lgQBs= " alt="" />
+
         </div>
     );
 };
